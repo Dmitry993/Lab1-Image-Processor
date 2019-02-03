7ce242b381d91292f7939c04a99ca4bed5001637
Дима Ярута <dima.yaruta@gmail.com>
2019-02-03 13:05:00 +0300

    Add project files.

---
+++ ImageProcessor/ImageProcessing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace ImageProcessor
+{
+    class ImageProcessing
+    {
+
+    }
+}
